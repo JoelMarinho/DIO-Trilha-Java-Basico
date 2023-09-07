6c63f2567fd258ca41c698ba98739521d57acf60
joelson marinho <joelsonjunior934s@gmail.com>
2023-09-07 17:31:28 -0300

    atualização

--- sistema-smart-tv/src/SmartTV.java
+++ sistema-smart-tv/src/SmartTV.java
@@ -8,22 +8,26 @@ public class SmartTV {
     }
 
     public void aumentarCanal() {
+        // canal = +1;
         canal++;
 
     }
 
     public void diminuirCanal() {
+        // canal = -1;
         canal--;
 
     }
 
     public void aumentarVolume() {
+        // volume = +1;
         volume++;
         System.out.println("Aumentar o volume para: " + volume);
 
     }
 
     public void diminuirVolume() {
+        // volume -1;
         volume--;
         System.out.println("Diminuir o volume para: " + volume);
 
